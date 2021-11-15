c107e171e76144f62a9f68defa11ac970d9caffa
emillyalmeida <emillyalmeidan@gmail.com>
2021-11-15 16:42:16 -0300

    jogo genius completo

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
+const { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } = require("constants");
+const { lookupService } = require("dns");
+
 let order = [];
 let Clickorder = [];
-let Score = 0;
+let score = 0;
 
 //0 = blue
 //1 = yellow
@@ -17,6 +20,86 @@ let shuffLeOrder = () => {
     order[order.length] = colorOrder;
     Clickorder = [];
     
+    for(let i in order){
+        let elementColor = createColorElement (order[i]);
+        lightcolor(elementColor, Number(i)+1);
 
+    }
 
 }
+
+let lightcolor=(element,number) => {
+    number = number *500;
+    setTimeout(()=>{
+        element.classList.add("selected");
+    }, number - 250);
+    setTimeout(()=>{
+        element.classList.remove("selected")});
+}
+
+let checkorder = ()=> {
+    for(let i in Clickorder){
+        if(Clickorder[1] != order[1]){
+            gameOver();
+            break;
+        }
+    }
+    if(Clickorder.length == order.length){
+        alert('pontuação: ${score}!\nParabéns! Você acertou! Vamos para o proximo nivel ')
+        nextLevel();
+    }
+}
+
+let click = (color)=> {
+    Clickorder[Clickorder.length] = color;
+    createColorElement(color).classList.add('selected');
+
+    setTimeout(() => {
+        createColorElement(color).classList.remove('selected');
+        checkorder();
+    },250);
+    
+
+}
+
+let createColorElement = (color) =>{
+    if(color == 0){
+        return blue;
+    }else if(color == 1){
+        return yellow;
+    }else if(color == 2){
+        return red;
+    }else if(color == 3){
+        return green;
+    }
+}
+
+let nextLevel = () =>{
+    score++;
+    shuffLeOrder();
+}
+
+
+let gameOver = ()=>{
+    alert('Pontuação: ${score}!\nVoçê perdeu o jogo\nClique me Ok para inciar um novp jogo');
+    order = [];
+    Clickorder= [];
+
+    playgame();
+}
+
+let playgame= () => {
+    alert('Bem vindo ao Gênesis! Iniciando um novo jogo')
+    score= 0;
+
+    nextLevel();
+}
+
+blue.onclick = () => click[0];
+yellow.onclick = () => click[1];
+red.onclick = () => click[2];
+green.onclick = () => click[3];
+
+
+
+playgame();
